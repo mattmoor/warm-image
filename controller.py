15a39075118ef910a98e05f1f25847bcb41e75cd
Matt Moore <mattmoor@google.com>
2017-10-27 16:18:41 +0000

    Add the create/delete semantics.

--- controller.py
+++ controller.py
@@ -26,18 +26,47 @@ class WarmImage(object):
     def __str__(self):
         return json.dumps(self._obj, indent=1)
 
+    def daemonset(self, owner_refs):
+        return {
+            "apiVersion": "extensions/v1beta1",
+            "kind": "DaemonSet",
+            "metadata": {
+                # TODO(mattmoor): Does this disambiguate enough?
+                "name": self.name(),
+                "ownerReferences": owner_refs,
+            },
+            "spec": {
+                "template": {
+                    "metadata": {
+                        "labels": {
+                            "name": self.name(),
+                        },
+                    },
+                    "spec": {
+                        "containers": [{
+                            "name": "the-image",
+                            "image": self.image(),
+                            # TODO(mattmoor): Do something better than this.
+                            "command": ["/bin/sh"],
+                            "args": ["-c", "sleep 10000000000"],
+                            # TODO(mattmoor): Support pull secrets.
+                        }],
+                    },
+                },
+            },
+        }
+
 
 def main():
     config.load_incluster_config()
 
-    api_ext = client.ApiextensionsV1beta1Api()
-    apps = client.AppsV1beta1Api()
+    apps_beta1 = client.AppsV1beta1Api()
     crds = client.CustomObjectsApi()
 
     # Create DaemonSets within our own namespace,
     # owned by us (so they go away if we do).
     namespace = os.environ["MY_NAMESPACE"]
-    owner = apps.read_namespaced_deployment(os.environ["OWNER_NAME"], namespace)
+    owner = apps_beta1.read_namespaced_deployment(os.environ["OWNER_NAME"], namespace)
 
     # Define our OwnerReference that we will add to the metadata of
     # objects we create so that they are garbage collected when this
@@ -51,14 +80,20 @@ def main():
         "uid": owner.metadata.uid,
     }
 
+    ext_beta1 = client.ExtensionsV1beta1Api()
+
     def create_meta(warmimage):
-        logging.error("Create the DaemonSet for: %s", str(warmimage))
+        ds = ext_beta1.create_namespaced_daemon_set(namespace, warmimage.daemonset([controller_ref]))
+        logging.error("Created the DaemonSet: %s", ds)
 
     def update_meta(warmimage):
-        logging.error("Update the DaemonSet for: %s", str(warmimage))
+        logging.error("TODO Update the DaemonSet for: %s", str(warmimage))
 
     def delete_meta(warmimage):
-        logging.error("Delete the DaemonSet for: %s", str(warmimage))
+        ext_beta1.delete_namespaced_daemon_set(
+            warmimage.name(), namespace, body=client.V1DeleteOptions(
+                propagation_policy='Foreground', grace_period_seconds=5))
+        logging.error("Deleted the DaemonSet for: %s", str(warmimage))
 
     def process_meta(t, warmimage, obj):
         if t == "DELETED":
